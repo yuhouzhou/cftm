7c7ab2696a40c0f8dc495d63c1ec213a391f7f03
yuhouzhou <zyh.germany@gmail.com>
2019-07-29 20:22:23 +0200

    change to import module as alias

--- code/visualization.py
+++
@@ -1,6 +0,0 @@
-import pickle
-import pyLDAvis.gensim
-
-lda = pickle.load(open('../model/lda_model', 'rb'))
-
-vis = pyLDAvis.gensim.prepare(lda['model'], lda['corpus'], dictionary=lda['dictionary'])
