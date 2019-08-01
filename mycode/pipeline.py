5886e0a9fe195eaeb83012de93d2e44ccfe66c56
Yuhou Zhou <zyh.germany@gmail.com>
2019-08-01 14:34:37 +0200

    add pipleline to argparser, put pathes in the front
    Former-commit-id: 4a996b8da1ec4772f8bf9396dfe1a3f8d5d9b295
    Former-commit-id: 53c09f9b5d3c52a2ee2db3299e1e6b0b00e7fb60

--- mycode/pipeline.py
+++ mycode/pipeline.py
@@ -16,58 +16,65 @@ import datetime
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-    '-p', '--preprocessing',
-    type=int, nargs='?', const=1, default=1,
-    help='Enter 1 to preprocess parquet files; 0 to use pickled training file'
+    'pipeline',
+    type=int, nargs=3, default=[1, 1, 1],
+    help=('Define pipeline: \n'
+          '    Require 3 numbers. Each number switches on or off a part of the pipeline.\n'
+          '    The first position is preprocessing,\n'
+          '    the second position is modelling,\n'
+          '    the third position is visualization.\n'
+          '    Enter 1 to turn on, 0 to turn off the specific part of pipeline.\n'
+          '    Example: 1 1 1 to run the full pipeline,\n'
+          '    1 1 0 to run preprocessing and modelling but without visualization.\n'
+          '    ')
 )
+
 parser.add_argument(
     '-o', '--observation_n',
-    type=int, nargs='?', const=-1,default=-1,
+    type=int, nargs='?', const=-1, default=-1,
 )
 parser.add_argument(
-    '-l', '--agg_length',
-    type=int, nargs='?', const=-1,default=-1
-)
-parser.add_argument(
-    '-m', '--modelling',
-    type=int, nargs='?', const=1, default=1,
-    help='Enter 1 to run lda models; 0 to use pickled lda model '
+    '-a', '--agg_length',
+    type=int, nargs='?', const=-1, default=-1
 )
 
 parser.add_argument(
     '-r', '--topic_range',
-    nargs=2, default=[10, 15],
-    type=int
-)
+    type=int, nargs=2, default=[10, 15],
 
-parser.add_argument(
-    '-v', '--visualise',
-    type=int, nargs="?", const=1, default=1,
-    help='Enter 1 to visualise the topic coherence and the model result; 0 to skip'
 )
-
 
 args = parser.parse_args()
 
+parquet_path1 = "../data.nosync/customer_feedbacks/" \
+                "part-00000-985ad763-a6d6-4ead-a6dd-c02279e9eeba-c000.snappy.parquet"
+parquet_path2 = '../data.nosync/customer_feedbacks_cat/' \
+                'part-00000-4820af87-4b19-4958-a7a6-7ed03b76f1b1-c000.snappy.parquet'
+data_path = '../output/training_data.pickle'
+model_path = '../output/lda_model_n_coherence_lst.pickle'
+pic_path = '../output/coherence.png'
+html_path = '../output/lda.html'
 dt = str(datetime.datetime.now().strftime('%Y%m%d_%H_%M_%S'))
 
-if args.preprocessing:
+if args.pipeline[0]:
     # Parsing
-    path1 = "../data.nosync/customer_feedbacks/part-00000-985ad763-a6d6-4ead-a6dd-c02279e9eeba-c000.snappy.parquet"
-    path2 = "../data.nosync/customer_feedbacks_cat/part-00000-4820af87-4b19-4958-a7a6-7ed03b76f1b1-c000.snappy.parquet"
-    df_pd = cftm_parser.parquet_transform(path1, path2, n=args.observation_n)
+    df_pd = cftm_parser.parquet_transform(parquet_path1, parquet_path2, n=args.observation_n)
 
     # Preprocessing
     stopwords = list(STOP_WORDS)
     texts, dictionary, corpus = pp.preprocessor(df_pd, stopwords=stopwords, language='de', text='TEXT', metadata='DATE',
                                                 min_len=args.agg_length)
     training_data = {"texts": texts, "dictionary": dictionary, "corpus": corpus}
-    pickle.dump(training_data, open('../output/training_data.pickle', 'wb'))
-else:
-    training_data = pickle.load(open('../output/training_data.pickle', 'rb'))
-    texts, dictionary, corpus = training_data['texts'], training_data['dictionary'], training_data['corpus']
+    pickle.dump(training_data, open(data_path, 'wb'))
+elif args.pipeline[1] or args.pipeline[2]:
+    try:
+        training_data = pickle.load(open(data_path, 'rb'))
+        texts, dictionary, corpus = training_data['texts'], training_data['dictionary'], training_data['corpus']
+    except FileNotFoundError:
+        print("Training Data Not Found!")
+        exit(1)
 
-if args.modelling:
+if args.pipeline[1]:
     # Model Generation
     lda_lst = []
     coherence_lst = []
@@ -88,28 +95,33 @@ if args.modelling:
     # Model Selection
     lda_pickle = {"model_lst": lda_lst, "coherence_lst": coherence_lst,
                   "n_topics_min": n_topic_min, "n_topics_max": n_topics_max}
-    pickle.dump(lda_pickle, open('../output/lda_model_n_coherence_lst.pickle', 'wb'))
-    pickle.dump(lda_pickle, open('../output/archive/lda_model_n_coherence_lst_'+dt+'.pickle', 'wb'))
-else:
-    lda_pickle = pickle.load(open('../output/lda_model_n_coherence_lst.pickle', 'rb'))
-    lda_lst, coherence_lst, n_topic_min, n_topics_max = lda_pickle.values()
+    pickle.dump(lda_pickle, open(model_path, 'wb'))
+    pickle.dump(lda_pickle, open(os.path.splitext(model_path) + dt + '.pickle', 'wb'))
+elif args.pipeline[2]:
+    try:
+        lda_pickle = pickle.load(open(model_path, 'rb'))
+        lda_lst, coherence_lst, n_topic_min, n_topics_max = lda_pickle.values()
+    except FileNotFoundError:
+        print("Model Not Found!")
+        exit(1)
 
-if args.visualise:
+if args.pipeline[2]:
     # Plot Topic Coherence
     index = int(np.argmin(coherence_lst))
     lda = lda_lst[index]
     plt.scatter(range(n_topic_min, n_topics_max + 1), coherence_lst)
     plt.scatter(n_topic_min + index, coherence_lst[index], color='r')
-    plt.annotate(str(n_topic_min + index) + ', ' + str(coherence_lst[index]), (n_topic_min + index, coherence_lst[index]))
+    plt.annotate(str(n_topic_min + index) + ', ' + str(coherence_lst[index]),
+                 (n_topic_min + index, coherence_lst[index]))
     plt.title('Topic Coherence vs. Number of Topics')
     plt.xlabel('Number of Topics')
     plt.ylabel('Topic Coherence (By UMass)')
     plt.tight_layout()
-    plt.savefig('../output/coherence.png')
-    if args.modelling:
-        plt.savefig('../output/archive/coherence_'+dt+'.png')
+    plt.savefig(pic_path)
+    if args.pipeline[1]:
+        plt.savefig(os.path.splitext(pic_path) + dt + '.png')
 
     # Data Visualization
     vis = pyLDAvis.gensim.prepare(lda, corpus, dictionary=dictionary)
-    pyLDAvis.save_html(vis, '../output/lda.html')
-    webbrowser.open(os.path.abspath('../output/lda.html'), new=2)
+    pyLDAvis.save_html(vis, html_path)
+    webbrowser.open(os.path.abspath(html_path), new=2)
