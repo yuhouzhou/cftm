9559729868147826636be21496e390a98cd7b3da
zhouy <yuhou.zhou@cewe.de>
2019-08-07 15:50:32 +0200

    add integration test
    Former-commit-id: 92e67367040a664ae2e6e258e4985b925d7e5a50

---
+++ test/test_pipeline.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Integration test for pipeline
+
+# get the directory of this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+touch integration_test_result.txt
+rm ./test_output/archive/*
+python ../cftm/pipeline.py 1 1 1 -p '../test/test_path.yaml' -o 30000 -a 300 -r 25 35 -s 2
+
+# clean up
+if [ "$(ls -1 ../test/test_output/archive | wc -l)" -ne 2 ]; then
+   echo "archive failed!" > ./integration_test_result.txt
+   rm ./test_output/archive/*
+else
+   echo "archive succeed!" > ./integration_test_result.txt
+   rm ./test_output/archive/*
+fi
