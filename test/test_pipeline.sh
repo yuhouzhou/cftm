c6873f452559588a1cf63875aa8cfebd60b41a62
zhouy <yuhou.zhou@cewe.de>
2019-08-21 11:52:25 +0200

    fix .gitignore
    Former-commit-id: 5c7331a427e76371ede9f6dc02f619a0fb4d6f68

--- test/test_pipeline.sh
+++ test/test_pipeline.sh
@@ -5,14 +5,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 touch integration_test_result.txt
-rm ./test_output/archive/*
+rm -r ./test_output/archive/*
 python ../cftm/pipeline.py 1 1 1 -p '../test/test_path.yaml' -o 30000 -a 300 -r 25 35 -s 2
 
 # clean up
-if [ "$(ls -1 ../test/test_output/archive | wc -l)" -ne 5 ]; then
+if [ "$(ls -1 ../test/test_output/archive/20*/* | wc -l)" -ne 5 ]; then
    echo "archive failed!" > ./integration_test_result.txt
-   rm ./test_output/archive/*
+   rm -r ./test_output/archive/*
 else
    echo "archive succeed!" > ./integration_test_result.txt
-   rm ./test_output/archive/*
+   rm -r ./test_output/archive/*
 fi
