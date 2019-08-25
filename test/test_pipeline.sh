b84d882814f04d808215300924a317d699b8d7d0
yuhouzhou <zyh.germany@gmail.com>
2019-08-25 22:36:33 +0200

    update environment file; update integrate test. parameter changed

--- test/test_pipeline.sh
+++ test/test_pipeline.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # Integration test for pipeline
 
 # get the directory of this file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
+cd $DIR || exit
 touch integration_test_result.txt
+mkdir -p ./test_output/archive
 rm -r ./test_output/archive/*
-python ../cftm/pipeline.py 1 1 1 -p '../test/test_path.yaml' -o 30000 -a 300 -r 1 102 5 -s 2
+python ../cftm/pipeline.py 1 1 1 -p '../test/test_path.yaml' -o 30000 -l 300 -r 1 102 10 -s 2 -c 300 -i 900
 
 # clean up
 if [ "$(ls -1 ../test/test_output/archive/20*/* | wc -l)" -ne 6 ]; then
